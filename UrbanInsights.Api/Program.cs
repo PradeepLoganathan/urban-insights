a9da36d40aff6a5dfc1c62cb9e363e9e56504718
Pradeep Loganathan <pradeep.loganathan@hotmail.com>
2024-06-26 13:22:58 +1000

    updated to minimal api and sln file

---
+++ UrbanInsights.Api/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<UrbanInsightsDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Sample endpoint
+app.MapGet("/", () => "Welcome to Urban Insights API!");
+
+// Add more endpoints here
+
+app.Run();
